1f86050e53ddbb6b08f21f79854e98bc31317d32
chiszo <gigibethandrea@gmail.com>
2024-06-08 16:05:04 -0500

    creación del proyecto

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/DswiiCl1SoapGrupo4Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapGrupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapGrupo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/DswiiCl1SoapGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapGrupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
