80038c463b42c62512ac8d88facf14094b741193
chiszo <gigibethandrea@gmail.com>
2024-06-08 18:10:00 -0500

    2 parte 2

--- src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/Pregunta2/ABisiesto.java
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/Pregunta2/ABisiesto.java
@@ -9,17 +9,17 @@ public class ABisiesto {
 
         Scanner scanner = new Scanner (System.in);
         System.out.println("Introduzca el año");
-        year =  Integer.valueOf(scanner.nextLine ());
+        year =  scanner.nextInt();
         if (year != 0){
             System.out.println(resultado(year));
         }
         else System.out.println("Error");
     }
 
-    public static Boolean isValid(int year){
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    private static Boolean isValid(int year){
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
-    public static String resultado(int year){
+    private static String resultado(int year){
         if(isValid(year)){
             return "El año " + year + " sí es bisiesto";
         } return "El año " + year + " no es bisiesto";
--- src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/Pregunta2/BPromedioAlumno.java
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/Pregunta2/BPromedioAlumno.java
@@ -1,7 +1,30 @@
 package pe.edu.cibertec.DSWII_CL1SOAP_grupo4.Pregunta2;
 
+import java.util.Scanner;
+
 public class BPromedioAlumno {
     public static void main(String[] args) {
+        double nota1 = 0, nota2 = 0, nota3 = 0;
 
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Introduzca 3 notas");
+
+        nota1 = scanner.nextDouble();
+        nota2 = scanner.nextDouble();
+        nota3 = scanner.nextDouble();
+
+        System.out.println(resultado(nota1, nota2, nota3));
+
+    }
+
+    private static double promedioAlumno(double nota1, double nota2, double nota3){
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    private static String resultado(double nota1, double nota2, double nota3){
+        if (promedioAlumno(nota1,nota2,nota3)>=70){
+            return "Aprobado";
+        }
+        return "Desaprobado";
     }
 }
