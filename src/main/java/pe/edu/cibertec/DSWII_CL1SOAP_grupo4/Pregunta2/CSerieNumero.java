dedbd8f86877403768e394832aec954258cf8801
chiszo <gigibethandrea@gmail.com>
2024-06-08 18:49:19 -0500

    2 parte 3

--- src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/Pregunta2/CSerieNumero.java
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_grupo4/Pregunta2/CSerieNumero.java
@@ -1,7 +1,29 @@
 package pe.edu.cibertec.DSWII_CL1SOAP_grupo4.Pregunta2;
 
+import java.util.Scanner;
+
 public class CSerieNumero {
     public static void main(String[] args) {
+        int limite=0;
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Introduzca un número límite");
+        limite = scanner.nextInt();
+        resultado(limite);
+    }
 
+    private static void resultado(int limite){
+        for (int i=1; i<=limite;i++){
+            System.out.println(i+" es "+isvalid(i));
+        }
+    }
+    //no cuenta el mismo número
+    private static boolean isvalid(int numero){
+        int suma = 0;
+        for (int i = 1; i <= numero / 2; i++) {
+            if (numero % i == 0) {
+                suma += i;
+            }
+        }
+        return suma == numero;
     }
 }
